3fd223feb3eccc4055807587825ec59aa164144c
webdiv-rakib <mdrakibulhassan24@gmail.com>
2024-10-22 06:19:02 +0600

    more conditional rendering ternary,&& and ||

--- src/Todo.jsx
+++ src/Todo.jsx
@@ -5,7 +5,7 @@
 //     )
 // }
 
-// conditional rendering option-1:
+// //conditional rendering option-1:
 // export default function Todo({ task, isDone }) {
 //     if (isDone === true) {
 //         return <li>Finished:{task}</li>
@@ -16,9 +16,30 @@
 // }
 
 // conditional rendering option-2:
+// export default function Todo({ task, isDone }) {
+//     if (isDone) {
+//         return <li>Finished:{task}</li>
+//     }
+//     return <li>Work on: {task}</li>
+// }
+
+// conditional rendering option-3:Ternary operator
+// export default function Todo({ task, isDone }) {
+//     return (
+//         <li>{isDone ? 'Finished' : 'Working On'}: {task}</li>
+//     )
+// }
+
+// conditional rendering option-4: &&
+// export default function Todo({ task, isDone }) {
+//     return (
+//         <li>{task}{isDone && ':Done'}</li>
+//     )
+// }
+
+// conditional rendering option-5: ||
 export default function Todo({ task, isDone }) {
-    if (isDone) {
-        return <li>Finished:{task}</li>
-    }
-    return <li>Work on: {task}</li>
+    return (
+        <li>{task}{isDone || ' : Do it'}</li>
+    )
 }
